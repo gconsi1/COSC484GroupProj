da68b55f7f050e3826e425aaffffe5780eb3e935
BLehosky <blehos1@students.towson.edu>
2024-04-13 13:38:50 -0400

    Added more CSS to manage the general layout on the home page, making the feed in the center of the page

--- homePageJS.js
+++ homePageJS.js
@@ -1,26 +1,4 @@
-// JavaScript to toggle Dropdown menu on Profile
-// document.addEventListener('DOMContentLoaded', function(){
-//     var createPostForm = document.getElementById("createPostForm");
-//     var postList = document.getElementById("postList");
-
-//     createPostForm.addEventListener('submit', function(event) {
-//         var postContent = document.getElementById("postContent").value;
-//         var newPost = document.createElement("div");
-//         newPost.classList.add("post");
-
-//         //Create a new post
-//         var postContentElement = document.createElement("p");
-//         postContentElement.classList.add("post-content");
-//         postContentElement.textContent = postContent;
-//         newPost.appendChild(postContentElement);
-
-//         //Add new post to the top of the feed
-//         postList.insertBefore(newPost, postList.firstChild);
-
-//         //Clear the form input after submission
-//         document.getElementById("postContent").value = "";
-//     })
-// });
+//JavaScript to create new post and siplay it in "Latest Posts" section
 document.addEventListener('DOMContentLoaded', function() {
     var createPostForm = document.getElementById("createPostForm");
     var postContent = document.getElementById("postContent");
