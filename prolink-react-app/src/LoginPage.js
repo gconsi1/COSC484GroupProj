3e3ecff2b60efe34b342d69bcf917398f5b248e1
Alexandro Nieto <anieto1@students.towson.edu>
2024-05-13 08:46:25 -0400

    Login Style

--- prolink-react-app/src/LoginPage.js
+++ prolink-react-app/src/LoginPage.js
@@ -44,24 +44,33 @@ const LoginPage = () =>{
             });
         }        
         
-
+        const inputStyle = {
+            width: '100%', 
+            padding: '15px',
+            margin: '8px 0',
+            display: 'inline-block',
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            boxSizing: 'border-box',
+            fontSize: '16px'
+        };
     return(
-            <div className="wrapper">
-                <form id="loginForm" onSubmit={handleLogin}>
+        <div className="wrapper" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', margin: 0, backgroundColor: '#f4f4f4', fontFamily: "Georgia, 'Times New Roman', Times, serif" }}>
+                <form id="loginForm" onSubmit={handleLogin}style={{ width: '80%', maxWidth: '600px', padding: '2em', backgroundColor: 'white', boxShadow: '0 0 20px rgba(0, 0, 0, 0.15)', borderRadius: '10px' }}>
                 <h1>Login</h1>
                 <div className="input-box">
                     <input type="text" placeholder="Username" onChange={e=>setUserId(e.target.value)}
-                    required />
+                    required style={inputStyle} />
                 </div>
                 <div className="input-box">
                     <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)}
-                    required />
+                    required style={inputStyle} />
                 </div>
                 <div className="remember">
                     <label><input type="checkbox" />Remember me</label>
                 </div>
-                <button type="submit" className="loginButton">Login</button>
-                <button className="to-signup"><Link to="/signup" className='signup-button'>
+                <button type="submit" className="loginButton"style={{ padding: '15px', width: '100%', padding: '15px', backgroundColor: '#72a98b', color: 'white', border: 'none', cursor: 'pointer', fontSize: '1em' }}>Login</button>
+                <button className="to-signup" style ={{alignItems: 'center'}}><Link to="/signup" className='signup-button'>
                             Sign up instead
                         </Link></button>
             </form>
